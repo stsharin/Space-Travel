7af4cadcd72d6ebb76bdeb09b89315e27c1e36e3
Saraban Tasnim Sharin <stssharin@gmail.com>
2022-05-08 15:45:53 +0600

    routes and searchbar added

---
+++ src/components/Routes/HandleRoutes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+import Home from '../Home/Home';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+
+const HandleRoutes = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default HandleRoutes;
